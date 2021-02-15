219cd9d4e190facde16c2ed8eb178baa58a056b1
Sergey1vstr <61208041+Sergey1vstr@users.noreply.github.com>
2021-02-15 13:55:53 +0300

    First commit

---
+++ lib/schedule_parser.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import re
+import numpy
+import pandas
+
+def tr_schedule(keywords, parameters, input_file, output_file):
+    """
+    Transforms an .inc file to .csv file
+    :param keywords:
+    :param parameters:
+    :param input_file:
+    :param output_file:
+    :return:
+    """
+    return
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from lib import schedule_parser as scp
+
+if __name__ == "__main__":
+    keywords = ("DATES", "COMPDAT", "COMPDATL")
+    parameters = ()
+    input_inc = "input_data/test_schedule.inc"
+    output_csv = "output_data/schedule.csv"
+
+    schedule_dataframe = scp.tr_schedule(keywords, parameters, input_file = input_inc, output_file = output_csv)
