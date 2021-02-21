2e5fa8bf75ab2a6659b9044b3fd61a201768539f
Sergey1vstr <61208041+Sergey1vstr@users.noreply.github.com>
2021-02-21 16:51:46 +0300

    Без комментариев

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-# -*- coding: utf-8 -*-
+from lib import schedule_lib as sl
 
-from lib import schedule_parser as scp
+schedule_name = input('Введите имя shedule файла: ')  # ввод пути файла shedule секции
+schedule_file = 'input' + '\\' + schedule_name
+print(schedule_file)
+count = 0
+
+with open(schedule_file, "r", encoding="UTF-8") as file:
+    line = file.readlines()  # считывание строк
+count = len(line)
+
+clean_schedule_name = 'handled_schedule.inc'
+# создает новый путь очищенного файла
+clean_schedule_file_path = "output" + '\\' + clean_schedule_name
 
 if __name__ == "__main__":
-    keywords = ("DATES", "COMPDAT", "COMPDATL")
-    parameters = ()
-    input_inc = "input_data/test_schedule.inc"
-    output_csv = "output_data/schedule.csv"
+    # список очищенных строк
+    line_clean = []
+    line_clean = sl.clean_schedule(line, clean_schedule_file_path, count)
+    count_clean = len(line_clean)
 
-    schedule_dataframe = scp.tr_schedule(keywords, parameters, input_file = input_inc, output_file = output_csv)
-    print("Пыщ пыщ ололо")
+    # список дат
+    data_list = []
+    data_list = sl.parse_keyword_DATE_line(line_clean, count_clean)
+    print("DATES", "\n", data_list)
+
+    # список COMPDAT
+    compdat_list = []
+    compdat_list = sl.parse_keyword_COMPDAT_line(line_clean, count_clean)
+    print("COMPDAT", "\n", compdat_list)
+
+    # список COMPDATL
+    compdatl_list = []
+    compdatl_list = sl.parse_keyword_COMPDATL_line(line_clean, count_clean)
+    print("COMPDATL", "\n", compdatl_list)
